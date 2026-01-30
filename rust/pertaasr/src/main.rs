0511ab89eb83a4be69f702f8833402bbfb946175
Himanshu Gupta <himanshu.mps@gmail.com>
2026-01-30 15:40:35 +0530

    Sample rust project

--- rust/pertaasr/src/main.rs
+++ rust/pertaasr/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use serde_json::Value;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let resp = reqwest::get("https://httpbin.org/ip").await?.json::<Value>().await?;
+    println!("{:#?}", resp);
+    Ok(())
 }
