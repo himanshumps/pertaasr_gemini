5302f5797207fd111d8099a1f947a4d8c9942880
Himanshu Gupta <himanshu.mps@gmail.com>
2026-02-04 10:15:58 +0530

    Update main.rs

--- test_server/src/main.rs
+++ test_server/src/main.rs
@@ -1,13 +1,13 @@
-use std::time::Duration;
+//use std::time::Duration;
 use actix_web::{get, App, HttpServer, Responder};
-use actix_web::rt::time::{sleep};
+//use actix_web::rt::time::{sleep};
 
-const DURATION: Duration = Duration::from_nanos(10);
+//const DURATION: Duration = Duration::from_nanos(10);
 
 #[get("/")]
 async fn index() -> impl Responder {
     // Introduce a 100 microsecond delay
-    sleep(DURATION).await;
+    //sleep(DURATION).await;
     "Hello world!"
 }
 
